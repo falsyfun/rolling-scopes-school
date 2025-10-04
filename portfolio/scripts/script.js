7c5be3a03dc9fb5415c2093e8965e97434d68cd4
falsyfun <>
2025-10-04 03:05:21 +0300

    feat: implement 'modal'

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -1,7 +1,13 @@
 "use strict";
 
+const OVERLAY = document.querySelector('.overlay');
+const MODAL = document.querySelector('.modal');
+const MODAL_CLOSE = document.querySelector('.modal__close');
+
 const NAV_TOGGLE = document.querySelector('#nav__toggle');
 const NAV_LINK = document.querySelectorAll('.nav__menu-link');
+
+const PRICE_BTN = document.querySelectorAll('.price__btn');
 
 //////////////////////////////
 
@@ -14,6 +20,18 @@ function enableScroll() {
 
 function disableScroll() {
   document.documentElement.classList.add('stop-scroll');
+}
+
+function showOverlay() {
+  OVERLAY.classList.add('overlay-show');
+  MODAL.classList.add('modal-show');
+  disableScroll();
+}
+
+function hideOverlay() {
+  OVERLAY.classList.remove('overlay-show');
+  MODAL.classList.remove('modal-show');
+  enableScroll();
 }
 
 function showMenu() {
@@ -29,13 +47,22 @@ function hideMenu() {
 // EVENTS
 //////////////////////////////
 
+OVERLAY.addEventListener('click', hideOverlay);
+
+MODAL_CLOSE.addEventListener('click', hideOverlay);
+
 NAV_TOGGLE.addEventListener('change', showMenu);
 
 NAV_LINK.forEach((elem) => {
   elem.addEventListener('click', hideMenu);
 });
 
+PRICE_BTN.forEach((elem) => {
+  elem.addEventListener('click', showOverlay);
+});
+
 window.addEventListener('resize', (event) => {
   NAV_TOGGLE.checked = false;
   enableScroll();
+  hideOverlay();
 });
